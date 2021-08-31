096a40393a93dee95721274aa028aa23f4dbdaed
Akash Arora <arora.akash7568@gmail.com>
2021-08-31 23:10:04 +0530

    Merging 2 pages

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -43,23 +43,19 @@
             </b-navbar-item>
         </template>
     </b-navbar>
-
-     <b-carousel
-            :progress="progress"
-            :progress-type="progressType">
-            <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
-                <section :class="`hero is-medium is-white`">
-                    <div class="hero-body has-text-centered">
-                        <h1 class="title">{{carousel.title}}</h1>
-                    </div>
-                </section>
-            </b-carousel-item>
-        </b-carousel>
-    <div class="columns">
-        <div class="column is-two-fifths">
-            <div class="container is-fluid">
-                <div class="notification is-primary">
-
+    <div class="columns form-area">
+       <div class="column is-one-fifth container is-fluid left-container notification is-primary">
+            <h1>
+              <!-- <i class='fas fa-book-reader'></i> -->
+              <div class = 'highlight'>
+              Upto 50% off on all Assignments
+              </div>
+            </h1>
+            <div class="puppy">
+              <img src="/static/img/idea.png" />
+            </div>
+          </div>
+        <div class="column is-two-fifths container is-fluid notification is-primary right-container">
                         <b-field label= "Name" horizontal>
                         <b-input v-model="name"></b-input>
                         </b-field>
@@ -101,10 +97,10 @@
                                 </div>
                                 </div> -->
                                     
-                                      <b-field grouped  label="Word Count" horizontal>
+                                      <b-field grouped  label="Words" horizontal >
             <b-input v-model="value" type="number"></b-input>
         </b-field>
-        <b-field>
+        <b-field label="Words" horizontal id="test">
             <b-slider v-model="value" lazy></b-slider>
         </b-field>
              <b-button rounded expanded>Submit</b-button>
@@ -140,14 +136,24 @@
                         </b-select>
                     </b-field> -->
                 </div>
-            </div>      
-        </div>
-        <div class="column is-half textcontainer">
+                  
+        <div class="column is-two-fifths container textcontainer">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus faucibus ornare suspendisse sed nisi. Ornare lectus sit amet est placerat in egestas. Suspendisse potenti nullam ac tortor vitae. Fringilla urna porttitor rhoncus dolor purus. Enim sit amet venenatis urna. Purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus. Duis convallis convallis tellus id interdum velit laoreet. Dolor magna eget est lorem. In nisl nisi scelerisque eu ultrices vitae auctor eu augue. Vitae tempus quam pellentesque nec nam aliquam sem et. Morbi tincidunt augue interdum velit euismod. Et odio pellentesque diam volutpat commodo sed. Porttitor massa id neque aliquam vestibulum. Malesuada fames ac turpis egestas sed tempus urna et pharetra. Sem et tortor consequat id porta nibh venenatis cras sed. Nisl purus in mollis nunc sed id. Aenean et tortor at risus viverra. Nunc eget lorem dolor sed viverra.
 
 Sit amet consectetur adipiscing elit duis tristique. Elementum pulvinar etiam non quam lacus. Malesuada fames ac turpis egestas maecenas pharetra convallis posuere morbi. Ultrices in iaculis nunc sed augue. Odio tempor orci dapibus ultrices in iaculis nunc. Sed felis eget velit aliquet sagittis id consectetur. Porta nibh venenatis cras sed felis eget velit aliquet sagittis. Ante in nibh mauris cursus mattis. Massa eget egestas purus viverra accumsan. Vel elit scelerisque mauris pellentesque pulvinar. Dapibus ultrices in iaculis nunc sed augue lacus viverra vitae. Scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper.
 </div>
   </div>
+   <b-carousel
+            :progress="progress"
+            :progress-type="progressType">
+            <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
+                <section :class="`hero is-medium is-white`">
+                    <div class="hero-body has-text-centered">
+                        <h1 class="title">{{carousel.title}}</h1>
+                    </div>
+                </section>
+            </b-carousel-item>
+        </b-carousel>
     </section>
 </template>
 
@@ -173,12 +179,12 @@ export default {
 <style>
 
 section {
-	background: #fafafa !important;
+	background: #ade8f4 !important;
 	margin: 0;
 }
 
-.notification.is-primary{
-    background: #7d97bd !important;
+.right-container{
+    background: #90e0ef !important;
 }
 .textcontainer {
     margin: 25px;
@@ -186,9 +192,56 @@ section {
 }
 
 .button.is-primary {
-    /* background-color: #7957d5; */
-    padding: 2%;
-    border-color: transparent;
-     color: #000; 
+  /* background-color: #7957d5; */
+  padding: 2%;
+  border-color: transparent;
+  color: #000;
+}
+.highlight {
+  background: #EAC4D5;
+  padding: 4px;
+}
+
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+.form-area {
+  background: #ade8f4 !important;
+}
+.left-container img {
+  /* filter: sepia(100%); */
+  width: 80%;
+}
+.left-container {
+   background: #caf0f8 !important;
+  overflow: hidden;
+  padding: 40px 0 0 0;
+  position: relative;
+  text-align: center;
+  width: 300px;
+}
+.left-container h1 {
+  color: rgba(0, 0, 0, 0.8);
+  display: inline-block;
+  font-size: 24px;
+}
+.left-container h1 i {
+  background: #caf0f8;
+  border-radius: 50%;
+  color: #ffffff;
+  font-size: 24px;
+  margin-right: 5px;
+  padding: 10px;
+  transform: rotate(-45deg);
+}
+.signup-container {
+  display: grid;
+  grid-template-areas: "left right";
+}
+#test label{
+  visibility: hidden;
 }
 </style>
